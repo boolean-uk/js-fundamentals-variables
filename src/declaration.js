5bb1a4ba88eb651de594d62cc86010518cb527c7
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2022-11-09 20:07:41 +0000

    answered

--- src/declaration.js
+++ src/declaration.js
@@ -3,6 +3,10 @@
 //
 // TODO: 1. Declare the variables firstName and age so that the tests pass
 
+let firstName = 'Jane'
+let age = 35
+
+
 // do not edit below this line
 let firstNameExport = ''
 try {
