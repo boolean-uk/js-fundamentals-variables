5aa2212676db9963b2322bd09d8da972bd901b81
dearshrewdwit <dearshrewdwit@gmail.com>
2023-04-04 11:28:05 +0100

    first step of declaration

--- src/declaration.js
+++ src/declaration.js
@@ -3,6 +3,12 @@
 //
 // TODO: 1. Declare the variables firstName and age so that the tests pass
 
+// do some work here
+
+const firstName = 'Jane'
+
+const age = 35
+
 // do not edit below this line
 let firstNameExport = ''
 try {
