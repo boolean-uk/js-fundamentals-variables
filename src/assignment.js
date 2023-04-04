6b695a198d0e862de0e7664e7400171406af3e33
Carmen Barcia <carmen.barcia@hotmail.com>
2023-04-04 16:52:54 +0200

    fixed the problems

--- src/assignment.js
+++ src/assignment.js
@@ -3,7 +3,7 @@ let firstNumber = 10
 firstNumber = 0
 
 // TODO: 1. Set the value of firstNumber below so the tests pass
-firstNumber = 20
+ firstNumber = 20
 
 
 // TODO: 2. Change the code below so that the tests pass
