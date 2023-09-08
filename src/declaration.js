c12c9fe0dccd3a3712d450ce2ab73d881ad4830d
Husnu Koci <husnukoci94@gmail.com>
2023-09-08 10:15:17 +0300

    declaration is done

--- src/declaration.js
+++ src/declaration.js
@@ -2,6 +2,8 @@
 //
 //
 // TODO: 1. Declare the variables firstName and age so that the tests pass
+const firstName = 'Koci'
+const age = 29
 
 // do not edit below this line
 let firstNameExport = ''
