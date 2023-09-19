1ba6c9e1e7f6cbdf984330a53189b27593540ddb
Mobeen <muhammadmobeen1@hotmail.com>
2023-09-19 17:45:18 +0200

    Code completed

--- src/declaration.js
+++ src/declaration.js
@@ -1,22 +1,8 @@
-//
-//
-//
 // TODO: 1. Declare the variables firstName and age so that the tests pass
-
+const firstName = 'Bob'
+const age = 26
+const firstName = 'Jane'
+const age = 35
+console.log(firstName, age)
 // do not edit below this line
 let firstNameExport = ''
-try {
-  /* eslint-disable no-undef */
-  firstNameExport = firstName
-} catch (e) {}
-
-let ageExport = 0
-try {
-  /* eslint-disable no-undef */
-  ageExport = age
-} catch (e) {}
-
-module.exports = {
-  firstName: firstNameExport,
-  age: ageExport
-}
