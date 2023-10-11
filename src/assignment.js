e926c12b88e583157fa7a020f7e1d80f9a199cc1
Loza_TM <lozatmamo@gmail.com>
2023-10-11 23:47:26 +0200

    Done

--- src/assignment.js
+++ src/assignment.js
@@ -1,15 +1,17 @@
-// do not edit this
-let firstNumber = 10
-firstNumber = 0
+// Do not edit this
+let firstNumber = 20;
+firstNumber = 0;
 
 // TODO: 1. Set the value of firstNumber below so the tests pass
-const firstName = 'Loza'
-console.log(firstName)
+firstNumber = 20;
+
 // TODO: 2. Change the code below so that the tests pass
-const secondNumber = 'Tadesse' // edit this value
-console.log(secondNumber)
-// do not edit the exported object.
+const secondNumber = 0; // edit this value
+// Change the value of secondNumber here
+const secondNumber = 42;
+
+// Do not edit the exported object.
 module.exports = {
   a: firstNumber,
   b: secondNumber
-}
+};
--- src/declaration.js
+++ src/declaration.js
@@ -1,27 +1,25 @@
-//
-//
-//
+const name='Jane'
+let otherName='Jane'
+
+//We can now assign a new value to name
+otherName='Bob'
+const age = 10
+const firstName = 'Jane'
 // TODO: 1. Declare the variables firstName and age so that the tests pass
-var firstName
-var age
-/* const firstName = 'Loza'
-const age = '26'
-console.log("My name is " + firstName + " and I am " + age + " years old.")* /
 // do not edit below this line
 let firstNameExport = ''
+let firstNameExport = 'Jane'
 try {
   /* eslint-disable no-undef */
   firstNameExport = firstName
-} try (e) {
-  firstName = 'Loza'
-}
+} catch (e) {}
 
 let ageExport = 0
+let ageExport = 35
 try {
   /* eslint-disable no-undef */
   ageExport = age
-} catch (e) {}
-
+}
 module.exports = {
   firstName: firstNameExport,
   age: ageExport
