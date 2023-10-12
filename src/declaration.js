047a3bde60ed24830d836394d67608e103c15765
Loza_TM <lozatmamo@gmail.com>
2023-10-12 14:36:40 +0200

    All fixed and tested

--- src/declaration.js
+++ src/declaration.js
@@ -3,7 +3,7 @@ let otherName='Jane'
 
 //We can now assign a new value to name
 otherName='Bob'
-const age = 10
+const age = 35
 const firstName = 'Jane'
 // TODO: 1. Declare the variables firstName and age so that the tests pass
 // do not edit below this line
