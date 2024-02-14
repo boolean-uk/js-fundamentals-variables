801130cfd2d3eb1297be62653f682df6139b2e7f
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-02-14 18:43:59 +0100

    changed the pass advanced solution

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -2,10 +2,11 @@ let a = 8
 let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
-a = a + 2
-console.log(a)
+a = a + b
+b = a - b
+a = a - b
 
-b = b - 2
+console.log(a)
 console.log(b)
 
 module.exports = { a, b }
