e549f625fd127aa4a87834b1ed642d2bee31fde0
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-02-14 18:26:24 +0100

    pass advanced

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,11 @@
-const a = 8
-const b = 10
+let a = 8
+let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+a = a + 2
+console.log(a)
+
+b = b - 2
+console.log(b)
 
 module.exports = { a, b }
