c68c4861098fb247d3fcbdb44e67dcbfb1595294
Shaun Harris <shaun@shaunwilliamharris.com>
2024-02-15 22:49:22 +0000

    JS_FV_ADV_PASS

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,11 +1,11 @@
 let a = 8
 let b = 10
 
+a = a + b
+b = a - b
+a = a - b
+
+
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
-module.exports = { a, b }[(a, (b = b), a)]
-
-+ a = a + b
-+ b = a - b
-+ a = a - b
-+
+module.exports = { a, b }
