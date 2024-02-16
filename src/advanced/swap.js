72b4b79c21cf923abaa80b1747995095a0698ec7
Mateusz Pietrzak <matti3301@gmail.com>
2024-02-16 10:33:04 +0100

    Fixed formatting

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,10 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+;[a, b] = [b, a]
+
+// or
+// a = 10;
+// b = 8;
+
 module.exports = { a, b }
