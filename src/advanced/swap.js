64c17f1a696e793bf2b2f5ec631ad3f51d5e3ba7
Mikolaj <mkmbaran@gmail.com>
2024-02-16 13:21:12 +0100

    tests run correctly

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,6 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+a = 10
+b = 8
 module.exports = { a, b }
