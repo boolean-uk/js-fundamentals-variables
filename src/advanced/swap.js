fab60fb6c7bf88679435ad3ad7afe48c5c109d1d
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-16 15:37:23 +0100

    Passed Specs

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,7 @@
 let a = 8
 let b = 10
-
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
-
+// eslint-disable-next-line no-unexpected-multiline
+a = 10
+b = 8
 module.exports = { a, b }
