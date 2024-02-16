47bc78916eb10423c3cce16cf965a2dfd4b587c9
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-02-16 11:31:33 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -2,5 +2,12 @@ let a = 8
 let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+let c = 0
+c = b
+b = a
+a = c
+
+console.log(a)
+console.log(b)
 
 module.exports = { a, b }
