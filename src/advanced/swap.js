07a04266887feff64e832555800fa6db018cda91
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-02-20 14:18:26 +0100

    Advaaanced. With a temp :/

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,8 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+const tempA = a
+a = b
+b = tempA
+
 module.exports = { a, b }
