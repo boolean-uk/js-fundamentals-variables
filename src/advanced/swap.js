fc1212f9b6df861af59e09361f09cd56a3b843f8
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-02-20 12:28:21 +0100

    all exercises should be finished

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,9 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+const c = a
+a = b
+b = c
+// [a, b] = [b, a]; // when attempting destructuring it just complains that the variables haven't been initialized
+
 module.exports = { a, b }
