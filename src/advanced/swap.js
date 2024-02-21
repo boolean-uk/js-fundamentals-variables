43e559da7e01cfd8a121a9bbf136c10e65324a83
Maha Riazi <maha.riazi@hotmail.no>
2024-02-21 13:33:44 +0100

    better swap

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -2,7 +2,8 @@ let a = 8
 let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
-a = 10
-b = 8
+a = a + b
+b = a - b
+a = a - b
 
 module.exports = { a, b }
